8709315176d9af36e6672b2bdd2193ef52a9cfbb
Pavlo Karpenko <karpenkopasha19@outlook.com>
2024-02-18 19:31:44 +0200

    Update burger menu

--- js/burger.js
+++ js/burger.js
@@ -1,22 +1,23 @@
-// function headerBurger() {
-//     let headerBburger = document.querySelector(".header__burger");
-//     let blockSocial = document.querySelector(".block-social");
-//     let blockMenuNavigation = document.querySelector('.blockMenu-navigation');
+function headerBurger() {
+    let headerBburger = document.querySelector(".header__burger");
+    let blockSocial = document.querySelector(".block-social");
+    let blockMenuNavigation = document.querySelector('.blockMenu-navigation');
+    let overlay = document.querySelector('.overlay');
   
-//     headerBburger.addEventListener("click", function() {
-//       let computedStyle = window.getComputedStyle(blockMenuNavigation);
+    headerBburger.addEventListener("click", function() {
+      let computedStyle = window.getComputedStyle(blockMenuNavigation);
   
-//       // Додаю та видаляю на burger при кліці клас
-//       this.classList.toggle("cross");
+      // Додаю та видаляю на burger при кліці клас
+      this.classList.toggle("cross");
   
-//       if (computedStyle.display === "none") {
-//         blockMenuNavigation.style.display = "block";
-//         blockSocial.style.display = "block";
-//       } else if (computedStyle.display === "block") {
-//         blockMenuNavigation.style.display = "none";
-//         blockSocial.style.display = "none";
-//       }
-//     });
-//   }
+      if (computedStyle.display === "none") {
+        blockMenuNavigation.style.display = "block";
+        blockSocial.style.display = "block";
+      } else if (computedStyle.display === "block") {
+        blockMenuNavigation.style.display = "none";
+        blockSocial.style.display = "none";
+      }
+    });
+  }
   
-//   headerBurger();
+  headerBurger();
